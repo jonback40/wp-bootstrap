b3aa1f55e001a4ef74068d9412b2b016016e5b24
Justin <32679969+justinb40@users.noreply.github.com>
2018-05-31 14:03:26 -0500

    Updated Layout

--- theme/single.php
+++ theme/single.php
@@ -1,28 +1,32 @@
 <?php get_header(); ?>
-<div class="page-main">
-    <div class="page-title">
-        <div class="container">
-            
-            <h1><?php echo get_bloginfo('name'); ?></h1>
-            
-        </div>
-    </div>
+<div class="page-main blog-detail">    
     <div class="inner">
         <div class="container">
+	        
+	        <div class="page-title">
+		        <h1><?php the_title(); ?></h1>
+		    </div>
             
             <?php if (have_posts()): ?>
                 <?php while (have_posts()): the_post(); ?>
                     <div class="post">
                         <div class="post-inner">
                             <div class="post-thumbnail"><?php the_post_thumbnail('full'); ?></div>
-                            <h3 class="post-title"><?php the_title(); ?></h3>
+<!--                             <h3 class="post-title"><?php the_title(); ?></h3> -->
                             <div class="post-author">By <?php the_author(); ?></div>
                             <div class="post-date">On <?php the_time('F jS, Y'); ?></div>
                             <div class="post-body"><?php the_content(); ?></div>
                         </div>
+                        
+                        <div class="post-share">
+	                        <div class="sharethis-inline-share-buttons sharethis"></div>
+                        </div>
+                        
                         <hr>
-                        <div class="post-comments-form"><?php comment_form(); ?></div>
+                        <div class="post-comments-form"><?php comment_form(); ?></div>                                                
+                        
                         <hr>
+                        
                         <div class="post-comments">
                             <?php
                             $comments = get_comments(array(
@@ -53,3 +57,5 @@
     </div>
 </div>
 <?php get_footer(); ?>
+
+<script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5ad0ee05dee63f0013b1bd60&product=inline-share-buttons' async='async'></script>
