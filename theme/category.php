19efb75c0719b90c6859e92293f6405c45df2fbb
Justin <32679969+justinb40@users.noreply.github.com>
2018-05-31 14:06:51 -0500

    Updates

--- theme/category.php
+++ theme/category.php
@@ -1,52 +1,45 @@
 <?php get_header(); ?>
-<div class="page-main">
-    <div class="page-title">
-        <div class="container">
-            
-            <h1>Categories</h1>
-            
-        </div>
-    </div>
+<div class="page-main category-index">    
     <div class="inner">
         <div class="container">
+	        
+	        <div class="page-title">
+		        <h1>Categories</h1>
+		    </div>
             
-            <?php if (have_posts()): ?>
-                <div class="posts">
-                    <?php while (have_posts()): the_post(); ?>
-                        <div class="post">
-                            <div class="post-inner">
-                                <div class="post-thumbnail">
-	                                <a href="<?php the_permalink(); ?>" title="Read More">
-		                                <?php the_post_thumbnail('medium'); ?>
-	                                </a>
+            <div class="row">
+	        	<div class="col-sm-8">
+		        	<?php if (have_posts()): ?>
+		                <?php while (have_posts()): the_post(); ?>
+		                    <div class="post">
+		                        <div class="post-inner">
+		                            <div class="post-thumbnail"><?php the_post_thumbnail('full'); ?></div>
+		                            <h3 class="post-title"><?php the_title(); ?></h3>
+		                            <div class="post-author">By <?php the_author(); ?></div>
+		                            <div class="post-date">On <?php the_time('F jS, Y'); ?></div>
+		                            <div class="post-body"><?php the_excerpt(); ?></div>
+		                            
+		                            <div class="post-links">
+			                            <div class="sharethis-inline-share-buttons sharethis"></div>
+			                            
+		                            	<a href="<?php echo get_permalink(); ?>" class="btn btn-default"> Read More</a>		                            										
+		                            </div>		                            
 		                        </div>
-                                <h3 class="post-title">
-	                                <a href="<?php the_permalink(); ?>" title="Read More">
-		                                <?php the_title(); ?>
-	                                </a>
-			                    </h3>
-                                <div class="post-author">By <?php the_author(); ?></div>
-                                <div class="post-date">On <?php the_time('F jS, Y'); ?></div>
-                                <div class="post-excerpt"><?php the_excerpt(); ?></div>
-                                <div class="post-links">
-                                    <a href="<?php the_permalink(); ?>" class="btn btn-default post-permalink" title="Read More">Read More</a>
-                                    <a href="<?php comments_link(); ?>" class="post-comments-link" title="View Comments">
-                                        <?php comments_number(); ?>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endwhile; ?>
-                </div>
-                
-                <nav class="pager">
-                    <?php pagination_bar(); ?>
-                </nav>
-            <?php else: ?>
-                <p><em>Sorry, no posts were found.</em></p>
-            <?php endif; ?>
+		                    </div>
+		                <?php endwhile; ?>
+		            <?php else: ?>
+		                <p><em>Sorry, no posts were found.</em></p>
+		            <?php endif; ?>	
+	        	</div>
+	        	
+	        	<div class="col-sm-4">
+		        	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	        	</div>
+	        </div>
             
         </div>
     </div>
 </div>
 <?php get_footer(); ?>
+
+<script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5ad0ee05dee63f0013b1bd60&product=inline-share-buttons' async='async'></script>
